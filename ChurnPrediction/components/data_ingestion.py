c67efe7782a7b7ac7afce18a4dc0557cf7238665
DaniyalM11 <DaniyalM11@hotmail.com>
2024-12-23 17:25:41 -0500

    data ingestion components

---
+++ ChurnPrediction/components/data_ingestion.py
@@ -0,0 +1,84 @@
+from ChurnPrediction.logging.logger import logging
+from ChurnPrediction.exception.exception import ChurnPredictionException
+from ChurnPrediction.constant import training_pipeline
+
+# Config of the data ingestion
+
+from ChurnPrediction.entity.config_entity import DataIngestionConfig
+from ChurnPrediction.entity.artifact_entity import DataIngestionArtifact
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+from typing import List
+from sklearn.model_selection import train_test_split
+
+class DataIngestion:
+    def __init__(self, data_ingestion_config: DataIngestionConfig):
+        try:
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise ChurnPredictionException(e,sys)
+        
+    def export_collection_as_dataframe(self):
+        try:
+            collection_name = self.data_ingestion_config.collection_name    
+
+            df=pd.read_csv(self.data_ingestion_config.filedrop_location + training_pipeline.FILE_NAME)
+            return df
+        except Exception as e:
+            raise ChurnPredictionException(e,sys)
+
+    def export_data_into_feature_store(self,df:pd.DataFrame):
+        try:
+            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
+            #createing folder
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            df.to_csv(feature_store_file_path,index=False.Header=True)
+            return df
+        
+        except Exception as e:
+            raise ChurnPredictionException(e,sys)
+        
+    def split_data_as_train_test(self,df:pd.DataFrame):
+        try:
+            train_set, test_set = train_test_split(df,
+                test_size=self.data_ingestion_config.train_test_split_ratio,random_state=training_pipeline.RANDOM_STATE)
+        
+            logging.info("Train and Test data split done")
+
+            logging.info("Exited split_data_as_train_test method of Data_Ingestion class")
+
+            #this dir_path will work for both train and test data
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+
+            os.makedirs(dir_path,exist_ok=True)
+
+            logging.info("exporting created for train and test data to filepath")
+
+            train_set.to_csv(self.data_ingestion_config.training_file_path,index=False,Header=True)
+
+            test_set.to_csv(self.data_ingestion_config.testing_file_path,index=False,Header=True)
+
+            logging.info("exported train and test data to filepath")    
+            
+        except Exception as e:
+            raise ChurnPredictionException(e,sys)
+        
+    def initiate_data_ingestion(self):
+        try:
+            df = self.export_collection_as_dataframe()
+            df = self.export_data_into_feature_store(df)
+            self.split_data_as_train_test(df)
+            dataingestionartifact = DataIngestionArtifact(self.data_ingestion_config.training_file_path,
+                                                          self.data_ingestion_config.testing_file_path)
+            return dataingestionartifact    
+            
+        except Exception as e:
+            raise ChurnPredictionException(e,sys)    
+
+                
+
+        
--- ChurnPrediction/constant/training_pipeline/__init__.py
+++ ChurnPrediction/constant/training_pipeline/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-from utils.main_utils import churn_filename
+from ChurnPrediction.utils.main_utils import churn_filename
 
 
 """
@@ -14,6 +14,10 @@ PIPELINE_NAME: str = "ChurnPrediction"
 ARTIFACTS_DIR: str = "Artifacts"
 FILE_NAME: str = churn_filename()
 
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+RANDOM_STATE: int = 42
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -21,4 +25,5 @@ DATA_INGESTION_COLLECTION_NAME: str = "ChurnPrediction"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
-DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+DATA_INGESTION_FILEDROP_LOCATION: str = "Telco Customer Data\\"
